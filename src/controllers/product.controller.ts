5a580cd99f95dfb935d36bc011f41a7caa783832
Jaloliddin18 <jaloliddin.khonimkulov05@gmail.com>
2026-01-04 04:44:34 +0900

    feat: create product controller and routing APIs

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,58 @@
+import { Request, Response } from 'express';
+import ProductService from '../models/Product.service';
+import Errors, { HttpCode, Message } from '../libs/Error';
+import { T } from '../libs/types/common';
+import { AdminRequest } from '../libs/types/member';
+
+const productService = new ProductService();
+const productController: T = {};
+
+productController.getAllProducts = async (req: Request, res: Response) => {
+	try {
+		console.log('getAllProducts');
+
+		res.render('products');
+	} catch (err) {
+		console.log('Error getAllProducts', err);
+		if (err instanceof Errors) res.status(err.code).json(err);
+		else res.status(Errors.standard.code).json(Errors.standard);
+	}
+};
+
+productController.createNewProduct = async (
+	req: AdminRequest,
+	res: Response,
+) => {
+	try {
+		console.log('createNewProduct');
+		if (!req.files?.length)
+			throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+
+		res.send(
+			`<script> alert("${'Successful Creation'}"); window.location.replace("/admin/product/all") </script>`,
+		);
+	} catch (err) {
+		console.log('Error createNewProduct', err);
+		const message =
+			err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+		res.send(
+			`<script> alert("${message}"); window.location.replace("/admin/product/all") </script>`,
+		);
+	}
+};
+
+productController.updateChosenProduct = async (req: Request, res: Response) => {
+	try {
+		console.log('updateChosenProduct');
+		const id = req.params.id;
+		console.log('id:', id);
+
+		res.status(HttpCode.OK).json();
+	} catch (err) {
+		console.log('Error updateChosenProduct', err);
+		if (err instanceof Errors) res.status(err.code).json(err);
+		else res.status(Errors.standard.code).json(Errors.standard);
+	}
+};
+
+export default productController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import coffeeshopController from './controllers/coffeeshop.controller';
+import productController from './controllers/product.controller';
 
 const routerAdmin = express.Router();
 
@@ -16,6 +17,9 @@ routerAdmin.get('/check-me', coffeeshopController.checkAuthSession);
 routerAdmin.get('/logout', coffeeshopController.logout);
 
 /** Products */
+routerAdmin.get('/product/all', productController.getAllProducts);
+routerAdmin.post('/product/create', productController.createNewProduct);
+routerAdmin.post('/product/:id', productController.updateChosenProduct);
 /** Users */
 
 export default routerAdmin;
