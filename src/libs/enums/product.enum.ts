7ca76c8ccf55ea971a0a7c46e017334cc93dd4da
Jaloliddin18 <jaloliddin.khonimkulov05@gmail.com>
2026-01-04 06:06:21 +0900

    feat: configure product schema model

---
+++ src/libs/enums/product.enum.ts
@@ -0,0 +1,20 @@
+export enum ProductSize {
+	SMALL = 'SMALL',
+	MEDIUM = 'MEDIUM',
+	LARGE = 'LARGE',
+	SET = 'SET',
+}
+
+export enum ProductStatus {
+	PAUSE = 'PAUSE',
+	PROCESS = 'PROCESS',
+	DELETE = 'DELETE',
+}
+
+export enum ProductCollection {
+	HOT_COFFEE = 'HOT_COFFEE',
+	ICED_COFFEE = 'ICED_COFFEE',
+	DESSERT = 'DESSERT',
+	SALAD = 'SALAD',
+	OTHER = 'OTHER',
+}
