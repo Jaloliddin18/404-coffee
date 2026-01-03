f4f9b4adcb0655f19e0028680dfcbce91fe7baa8
Jaloliddin18 <jaloliddin.khonimkulov05@gmail.com>
2026-01-03 09:14:39 +0900

    feat: frontend related css, js, and img creation

--- src/controllers/coffeeshop.controller.ts
+++ src/controllers/coffeeshop.controller.ts
@@ -10,7 +10,7 @@ const coffeeshopController: T = {};
 coffeeshopController.goHome = (req: Request, res: Response) => {
 	try {
 		console.log('goHome');
-		res.send('Home Page');
+		res.render('home');
 	} catch (err) {
 		console.log('Error, goHome', err);
 	}
@@ -18,7 +18,7 @@ coffeeshopController.goHome = (req: Request, res: Response) => {
 coffeeshopController.getSignup = (req: Request, res: Response) => {
 	try {
 		console.log('getSignup');
-		res.send('Signup Page');
+		res.render('signup');
 	} catch (err) {
 		console.log('Error, getSignup', err);
 	}
@@ -26,7 +26,7 @@ coffeeshopController.getSignup = (req: Request, res: Response) => {
 coffeeshopController.getLogin = (req: Request, res: Response) => {
 	try {
 		console.log('getLogin');
-		res.send('Login Page');
+		res.render('login');
 	} catch (err) {
 		console.log('Error, getLogin', err);
 	}
