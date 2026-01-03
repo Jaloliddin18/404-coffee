9b4e227e53132ea34f5c14ef30f8e6a4c72cc6a4
Jaloliddin18 <jaloliddin.khonimkulov05@gmail.com>
2026-01-04 08:01:38 +0900

    modify updateChosenProduct business logic

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,9 +1,10 @@
-import { Product } from '../libs/types/product';
+import { Product, ProductUpdateInput } from '../libs/types/product';
 import ProductModel from '../schema/Product.model';
 import { ProductInput } from '../libs/types/product';
 import { HttpCode } from '../libs/Error';
 import Errors from '../libs/Error';
 import { Message } from '../libs/Error';
+import { shapeIntoMongooseObjectId } from '../libs/config';
 
 class ProductService {
 	private readonly productModel;
@@ -18,6 +19,19 @@ class ProductService {
 			console.log('Error, model: createNewProduct', err);
 			throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
 		}
+	}
+
+	public async updateChosenProduct(
+		id: string,
+		input: ProductUpdateInput,
+	): Promise<Product> {
+		id = shapeIntoMongooseObjectId(id);
+		const result = this.productModel
+			.findByIdAndUpdate({ _id: id }, input, { new: true })
+			.exec();
+
+		if (!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED);
+		return result;
 	}
 }
 
