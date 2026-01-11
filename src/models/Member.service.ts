34ad219e539fd38257a183c5b14cd3c4049fc7c5
Jaloliddin18 <jaloliddin.khonimkulov05@gmail.com>
2026-01-11 16:38:46 +0900

    fix: modify spa login API process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -7,7 +7,7 @@ import {
 } from '../libs/types/member';
 import Errors, { Message } from '../libs/Error';
 import { HttpCode } from '../libs/Error';
-import { MemberType } from '../libs/enums/member.enum';
+import { MemberStatus, MemberType } from '../libs/enums/member.enum';
 import { shapeIntoMongooseObjectId } from '../libs/config';
 import * as bycript from 'bcryptjs';
 
@@ -33,11 +33,17 @@ class MemberService {
 		//TODO consider member status later
 		const member = await this.memberModel
 			.findOne(
-				{ memberNick: input.memberNick },
-				{ memberNick: 1, memberPassword: 1 },
+				{
+					memberNick: input.memberNick,
+					memberStatus: { $ne: MemberStatus.DELETE },
+				},
+				{ memberNick: 1, memberPassword: 1, memberStatus: 1 },
 			)
 			.exec();
 		if (!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
+		else if (member.memberStatus === MemberStatus.BLOCK) {
+			throw new Errors(HttpCode.FORBIDDEN, Message.BLOCKED_USER);
+		}
 
 		const isMatch = await bycript.compare(
 			input.memberPassword,
