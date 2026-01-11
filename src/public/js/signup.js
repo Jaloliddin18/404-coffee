42095e9c70ed0728a1e8eee1aae6565e807ec94a
Jaloliddin18 <jaloliddin.khonimkulov05@gmail.com>
2026-01-11 12:37:17 +0900

    feat: develop image pre loading on signup page

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,6 +1,28 @@
 console.log('Signup frontend javascript file');
 
-$(function () {});
+$(function () {
+	const fileTarget = $('.file-box .upload-hidden');
+	let filename;
+	fileTarget.on('change', function () {
+		if (window.FileReader) {
+			const uploadFile = $(this)[0].files[0];
+			const fileType = uploadFile['type'];
+			const validImageType = ['image/jpg', 'image/jpeg', 'image/png'];
+			if (!validImageType.includes(fileType)) {
+				alert('Please insert only jpeg, jpg,or png file type');
+			} else {
+				if (uploadFile) {
+					console.log(URL.createObjectURL(uploadFile));
+					$('.upload-img-frame')
+						.attr('src', URL.createObjectURL(uploadFile))
+						.addClass('success');
+				}
+				filename = $(this)[0].files[0].name;
+			}
+			$(this).siblings('.upload-name').val(filename);
+		}
+	});
+});
 function validateSignupForm() {
 	const memberNick = $('.member-nick').val();
 	const memberPhone = $('.member-phone').val();
@@ -16,8 +38,16 @@ function validateSignupForm() {
 		alert('Please insert all required inputs!');
 		return false;
 	}
-	if (memberNick !== confirmPassword) {
+	if (memberPassword !== confirmPassword) {
 		alert('Password differs, please check!');
+		return false;
+	}
+
+	const memberImage = $('.member-image').get(0).files[0]
+		? $('.member-image').get(0).files[0].name
+		: null;
+	if (!memberImage) {
+		alert('Please insert coffe shop image');
 		return false;
 	}
 }
